643f2b2aa590bb8f93cc2b51991d2b5ad1050b8a
alex <chenzeyongjsj@163.com>
2018-02-28 17:20:49 +0800

    init

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,11 @@
 /*
  * @Author: Alex chenzeyongjsj@163.com 
  * @Date: 2018-02-28 16:42:39 
- * @Last Modified by:   Alex chenzeyongjsj@163.com 
- * @Last Modified time: 2018-02-28 16:42:39 
+ * @Last Modified by: Alex chenzeyongjsj@163.com
+ * @Last Modified time: 2018-02-28 17:03:48
  */
 <template>
-
+  <img src="../../static/img/logo.png" alt="">
 </template>
 
 <script>
@@ -19,5 +19,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+img {
+  width: 1.8rem;
+}
 </style>
