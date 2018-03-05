5dcd9710e7fc7c814051ba8210f9077a101f3d8c
alex <chenzeyongjsj@163.com>
2018-03-05 16:44:18 +0800

    login done

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,24 @@
 /*
  * @Author: Alex chenzeyongjsj@163.com 
  * @Date: 2018-02-28 16:43:09 
- * @Last Modified by:   Alex chenzeyongjsj@163.com 
- * @Last Modified time: 2018-02-28 16:43:09 
+ * @Last Modified by: Alex chenzeyongjsj@163.com
+ * @Last Modified time: 2018-03-05 16:22:01
  */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
+import Login from '@/pages/Login' //登录页
+import Home from '@/pages/Home' //首页
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    }
-  ]
+    routes: [{
+            path: '/',
+            component: Login
+        },
+        {
+            path: '/pages/Home',
+            component: Home
+        }
+    ]
 })
